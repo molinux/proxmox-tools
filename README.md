b62cb66507cb0c079704b6df24ca3ef9d92cbcd9
Marcus Molinero <cybercrowbr@gmail.com>
2023-10-09 10:38:10 -0300

    First version. Initial version of proxmox-ct-template-prepare script

---
+++ README.md
@@ -0,0 +1,3 @@
+# PROXMOX TOOLS
+
+**proxmox-ct-template-prepare.sh** - Script to prepare a proxmox container before transform it in a CT Template
---
+++ proxmox-ct-template-prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -   
+#title          :proxmox-ct-template-prepare.sh
+#description    :Script to prepare a proxmox container before transform it in a CT Template
+#author         :Marcus Molinero
+#date           :20231009
+#version        :0.0.1  
+#usage          :./proxmox-ct-template-prepare.sh
+#notes          :       
+#bash_version   :4.4.23(1)-release
+#============================================================================
+
+echo "Digite o nome de usuario: "
+read -r USER
+adduser "$USER"
+usermod -aG sudo "$USER"
+
+apt-get update
+apt-get upgrade
+apt autoremote
+apt clean
+
+rm /etc/ssh/ssh_host_*
+truncate -s 0 /etc/machine-id
